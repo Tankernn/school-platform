3fa9af38395ac469eccae43700f82124beee62d9
Frans Bergman <frans@tankernn.eu>
2017-12-10 16:22:10 +0100

    Add full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get home_url
     assert_response :success
+    assert_select "title", "Home | School Platform"
   end
 
   test "should get about" do
     get about_url
     assert_response :success
+    assert_select "title", "About | School Platform"
   end
 
 end
