136f3713c1c54cb7c39a42d5e104261eaf0ae82d
Frans Bergman <frans@tankernn.eu>
2017-12-27 12:38:53 +0100

    Add default profile pictures

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -14,12 +14,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url(*args)
+    "/images/fallback/" + [version_name, "default_profile.png"].compact.join('_')
+  end
 
   # Create different versions of your uploaded files:
   version :tiny do
