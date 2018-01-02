bd28538b06b660f54c1498e1d7704cded5e6f861
Frans Bergman <frans@tankernn.eu>
2018-01-02 13:11:04 +0100

    Make URIs in text clickable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  def format_content(text)
+    simple_format(text).gsub(/([A-z]+:[^\s<]+)/, '<a href="\0">\0</a>').html_safe
+  end
 end
